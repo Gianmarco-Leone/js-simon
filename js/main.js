cd0c722e7d16a17a4a3a75a186f951746d79abee
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-02-03 14:46:53 +0100

    init repo

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// TRACCIA:
+
+// Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
+
+// Ogni secondo il nostro countdown dovrà scalare fino alle 9: 30 di domani mattina!
+
+// SVOLGIMENTO:
+
+let messageEl = document.getElementById("output_message");
+// Creare data fine countdown
+
+
+
+
+// Funzione per decrementare countdown ogni secondo
+
+// Impostare momento partenza countdown
+// Calcola giorno
+// Calcola ora
+// Calcola minuti
+// Calcola secondi
+
+// Impostare distanza tra momento partenza e fine countdown
+
+// SE distanza < 0 fine countdown
