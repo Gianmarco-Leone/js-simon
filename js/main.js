295e9b62217b68d32e7b1cf7876987430f3e8abd
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-02-03 15:17:07 +0100

    add sec, min, hours, days

--- js/main.js
+++ js/main.js
@@ -7,19 +7,45 @@
 // SVOLGIMENTO:
 
 let messageEl = document.getElementById("output_message");
-// Creare data fine countdown
-
-
-
-
-// Funzione per decrementare countdown ogni secondo
-
-// Impostare momento partenza countdown
-// Calcola giorno
-// Calcola ora
-// Calcola minuti
-// Calcola secondi
+// Creare data fine countdown (quanto tempo sarà passato fino al giorno n)
+const countDownDay = new Date("Feb 04, 2023 09:30:00").getTime();
+// console.log(countDownDay);
 
 // Impostare distanza tra momento partenza e fine countdown
 
 // SE distanza < 0 fine countdown
+
+
+
+const now = new Date().getTime();
+// console.log(now);
+
+let countDown = countDownDay - now;
+console.log("Millisecondi rimanenti al giorno n " + countDown);
+
+// Calcola secondi
+const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+console.log("Secondi rimanenti al minuto successivo o alla fine del countdown: " + seconds);
+
+// Calcola minuti
+const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+console.log("Minuti rimanenti all'ora successiva o alla fine del countdown: " + minutes);
+
+// Calcola ore
+const hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+console.log("Ore rimanenti al giorno successivo o alla fine del countdown: " + hours);
+
+// Calcola giorno
+const days = Math.floor((countDown / (1000 * 60 * 60 * 24)));
+console.log("Giorni rimanenti alla fine del countdown: " + days);
+
+
+// Funzione per decrementare countdown ogni secondo
+
+    // Impostare momento partenza countdown
+
+    // Calcola secondi
+    // const seconds = ;
+    // Calcola ora
+    // Calcola minuti
+    // Calcola giorno
